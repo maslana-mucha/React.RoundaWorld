cec7aaeb852d5c9610b38d4a620efaedd92cad1a
maslana.m <maslana.m89@gmail.com>
2020-05-26 18:56:32 +0200

    Improving animated switch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import { connect } from 'react-redux';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { AnimatedSwitch } from 'react-router-transition';
 import PropTypes from 'prop-types';
 
@@ -17,28 +17,28 @@ import NotFound from './components/views/NotFound/NotFound';
 
 import styles from './App.scss';
 import parseTrips from './utils/parseTrips';
-import {setMultipleStates} from './redux/globalRedux';
+import { setMultipleStates } from './redux/globalRedux';
 
 class App extends React.Component {
   static propTypes = {
     trips: PropTypes.array,
     setStates: PropTypes.func,
-  }
+  };
 
-  constructor(props){
+  constructor(props) {
     super(props);
     // parse trips when App is first created
     parseTrips(this.props.trips, this.props.setStates);
   }
 
-  componentDidUpdate(prevProps){
-    if(prevProps.trips != this.props.trips){
+  componentDidUpdate(prevProps) {
+    if (prevProps.trips != this.props.trips) {
       // parse trips again if they changed
       parseTrips(this.props.trips, this.props.setStates);
     }
   }
 
-  render(){
+  render() {
     return (
       <BrowserRouter>
         <MainLayout>
@@ -66,12 +66,12 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   trips: state.trips,
 });
 
-const mapDispatchToProps = dispatch => ({
-  setStates: newState => dispatch(setMultipleStates(newState)),
+const mapDispatchToProps = (dispatch) => ({
+  setStates: (newState) => dispatch(setMultipleStates(newState)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
