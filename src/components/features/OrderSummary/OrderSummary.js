5d8aad6888b9f83f03bc40765532e5fcc2de5caf
maslana.m <maslana.m89@gmail.com>
2020-06-10 12:49:12 +0200

    Connecting Total Price with redux state

---
+++ src/components/features/OrderSummary/OrderSummary.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { calculateTotal } from '../../../utils/calculateTotal';
+import { formatPrice } from '../../../utils/formatPrice';
+
+import styles from './OrderSummary.scss';
+
+const OrderSummary = ({ tripCost, options }) => (
+  <h2 className={styles.component}>
+    Total: <strong>{calculateTotal(formatPrice(tripCost), options)}</strong>
+  </h2>
+);
+
+OrderSummary.propTypes = {
+  tripCost: PropTypes.string,
+  options: PropTypes.object,
+};
+
+export default OrderSummary;
