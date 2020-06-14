2743e37e6e5f3c395618c92c025f0aaf9dac1d71
maslana.m <maslana.m89@gmail.com>
2020-06-14 20:39:15 +0200

    Adding ppl number option

--- src/components/features/OrderOption/OrderOptionNumber.js
+++ src/components/features/OrderOption/OrderOptionNumber.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './OrderOption.scss';
 
-const OrderOptionNumber = () => (
-  <div>
-    <h3></h3>
+//import { formatPrice } from '../../../utils/formatPrice';
+
+const OrderOptionNumber = ({ currentValue, setOptionValue, price }) => (
+  <div className={styles.number}>
+    <input
+      type="number"
+      className={styles.inputSmall}
+      value={currentValue}
+      min="1"
+      max="9"
+      onChange={(event) => setOptionValue(event.target.value)}
+    ></input>
+    {price}
   </div>
 );
+
+OrderOptionNumber.propTypes = {
+  currentValue: PropTypes.string,
+  setOptionValue: PropTypes.func,
+  price: PropTypes.string,
+};
 
 export default OrderOptionNumber;
