dfb955fad900f84744467d0edd50b690f7c1a36b
maslana.m <maslana.m89@gmail.com>
2020-06-18 20:16:31 +0200

    Change logo styling

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -10,25 +10,39 @@ class Header extends React.Component {
       <header className={styles.component}>
         <Grid>
           <Row between="md" middle="xs">
-            <Col md={3} lg={2}>
-              <Link to='/'>
+            <Col md={3} lg={3}>
+              <Link to="/">
                 <div className={styles.logo}>
-                  <Icon name='compass' />
-                  <span className={styles.name}>Travel Agency</span>
+                  <span className={styles.name}>
+                    R
+                    <span>
+                      <Icon name="compass" />
+                    </span>
+                    undaWorld
+                  </span>
                 </div>
               </Link>
             </Col>
             <Col md={6}>
               <nav>
-                <NavLink to='/trips' activeClassName='active'>Trips</NavLink>
-                <NavLink to='/countries' activeClassName='active'>Countries</NavLink>
-                <NavLink to='/regions' activeClassName='active'>Regions</NavLink>
-                <NavLink to='/info' activeClassName='active'>Contact</NavLink>
+                <NavLink to="/trips" activeClassName="active">
+                  Trips
+                </NavLink>
+                <NavLink to="/countries" activeClassName="active">
+                  Countries
+                </NavLink>
+                <NavLink to="/regions" activeClassName="active">
+                  Regions
+                </NavLink>
+                <NavLink to="/info" activeClassName="active">
+                  Contact
+                </NavLink>
               </nav>
             </Col>
             <Col md={3} lg={2}>
               <div className={styles.contact}>
-                <Icon name='phone' /><span>678.243.8455</span>
+                <Icon name="phone" />
+                <span>678.243.8455</span>
               </div>
             </Col>
           </Row>
